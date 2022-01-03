8e233e1672ad1a8e90af65af33d9343b331dd9f8
elvishn <elvis1042.rodriguez@gmail.com>
2022-01-03 16:13:06 -0600

    calculando el area y perimetro de figuras geometricas

--- figuras.js
+++ figuras.js
@@ -1,1 +1,33 @@
-console.log("Hello world");
+// Codigo del cuadrado:
+
+const ladoCuadrado = 5;
+
+const perimetroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+// Codigo del triangulo:
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+const areaTriangulo = (alturaTriangulo * baseTriangulo) / 2;
+
+// Codigo del circulo:
+
+const radioCirculo = 4;
+const diametroCirculo = radioCirculo * 2;
+const PT = Math.PI; // Aqui traemos el numero del PI original.
+
+const perimetroCirculo = diametroCirculo * PI;
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+
+/*
+Esto sirve para encapsular los console.log
+#Abres
+console.group("nombre que desees");
+#Cierras
+console.groupEnd();
+*/
