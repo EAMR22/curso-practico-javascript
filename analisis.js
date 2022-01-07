77595e763a45402235abd773fa8505c5f8d393b8
elvishn <elvis1042.rodriguez@gmail.com>
2022-01-07 11:55:22 -0600

    terminando el taller #4

--- analisis.js
+++ analisis.js
@@ -1,3 +1,7 @@
+function esPar(numerito){
+    (numerito % 2 === 0);
+}
+
 function calcularMediaAritmetica(lista){
     const sumaLista = lista.reduce( 
         function(valorAcumulado = 0, nuevoElemento){
@@ -10,21 +14,7 @@ function calcularMediaAritmetica(lista){
     return promedioLista;
 }
 
-const salariosCol = colombia.map(
-    function(personita){
-        return personita.salary;
-    }
-);
-
-const salariosColSorted = salariosCol.sort(
-    function(salaryA, salaryB){
-        return salaryA - salaryB;
-    }
-);
-
-function esPar(numerito){
-    (numerito % 2 === 0);
-}
+//calculadora de medianas
 
 function medianaSalarios(lista){
     const mitad = parseInt(lista.length / 2);
@@ -41,4 +31,35 @@ function medianaSalarios(lista){
     }
 }
 
-console.log(medianaSalarios(salariosColSorted));
+// Mediana general
+
+const salariosCol = colombia.map(
+    function(personita){
+        return personita.salary;
+    }
+);
+
+const salariosColSorted = salariosCol.sort(
+    function(salaryA, salaryB){
+        return salaryA - salaryB;
+    }
+);
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+//Mediana top 10%
+
+const spliceStart = (salariosColSorted.length * 90) / 100;
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(
+    spliceStart,
+    spliceCount,
+); // Con splice aqui parte el arreglo desde el 90% del contenido del array, hasta donde se termina.
+
+const medianaColTop10 = medianaSalarios(salariosColTop10);
+
+console.log({
+    medianaGeneralCol,
+    medianaColTop10,
+});
